646d1a476b54e6a6a90991b26e901fccd50f8d71
samercier <merciers.merciers@gmail.com>
2013-11-27 20:14:33 -0600

    started branding and funness

--- js/scoutify.js
+++ js/scoutify.js
@@ -4,12 +4,12 @@ function parse_file_new(data) {
        new_team(dragon.number, dragon.matches, dragon.name, dragon.value);
    });
 }
-function new_team(team_number, dragons, name, rank) {
-    $("#accordion").append("<p></p><div class='panel panel-default'><div class='panel-heading'><h4 class='panel-title'><a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion'href='#collapse" + team_number + "'>"+ rank +". Team " + team_number +": " + name + "</a></h4></div><div id='collapse" + team_number + "'class='panel-collapse collapse'> <div class='panel-body' style='overflow:auto;'><table class='table table-striped'><thead><tr id='erste" + team_number + "'><th>Team Number</th></tr></thead><tbody id='dritte" + team_number + "'><tr id='zweite"+ team_number +"0'><td>" + team_number+"</td></tr></tbody></table></div></div>" );
-    $.map(dragons, function(unicorn,match_number){
+function new_team(team_number, matches, name, rank) {
+    $("#accordion").append("<p></p><div class='panel panel-default'><div class='panel-heading'><h4 class='panel-title'><a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion'href='#collapse" + team_number + "'>"+ rank +". Team " + team_number +": " + name + "</a></h4></div><div id='collapse" + team_number + "'class='panel-collapse collapse'> <div class='panel-body table-body' style='overflow:auto;'><table class='table table-striped'><thead><tr id='erste" + team_number + "'><th>Team Number</th></tr></thead><tbody id='dritte" + team_number + "'><tr id='zweite"+ team_number +"0'><td>" + team_number+"</td></tr></tbody></table></div></div>" );
+    $.map(matches, function(match,match_number){
       if(match_number == 0) {
 	//if i == 0 for if first
-	$.map(unicorn, function(header,key) {
+	$.map(match, function(header,key) {
 	  //dritte + vierte
           $("#erste" + team_number ).append("<th>" + key +"</th>");
 	  $("#zweite" + team_number + match_number).append("<td>" + header + "</td>");
@@ -17,7 +17,7 @@ function new_team(team_number, dragons, name, rank) {
       }
       else {
 	 $("#dritte" + team_number).append("<tr id='zweite"+ team_number + match_number + "'><td>" + team_number+ "</td></tr>");
-	 $.map(unicorn, function(header,key) {
+	 $.map(match, function(header,key) {
 	     $("#zweite" + team_number + match_number).append("<td>" + header + "</td>");
 	 });
       }
