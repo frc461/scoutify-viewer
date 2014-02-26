b345aeb9030827f4122fb9cc397eba11b4d0dcbe
samercier <merciers.merciers@gmail.com>
2014-02-25 19:48:49 -0500

    Expand button and draw button

--- js/scoutify.js
+++ js/scoutify.js
@@ -6,16 +6,26 @@ function parse_file_new(data) {
 		new_team(dragon.number, dragon.matches, dragon.name, dragon.value);
 	});
 }
+
+function expand_all() {
+	console.log($(".accordion-toggle").each(function(e,t){t.click();t.click();t.click()}));
+}
+
+function expand_and_draw_graphs() {
+	console.log($(".accordion-toggle").each(function(e,t){t.click();t.click();t.click()}));
+	console.log($(".graph").each(function(e,t){t.click()}));
+	console.log($(".accordion-toggle").each(function(e,t){t.click()}));
+}
+
 function new_team(team_number, matches, name, rank) {
     var graph_array = [];
     var graph_count = 0;
     $("#accordion").append("<p></p><div class='panel panel-default'><div class='panel-heading'><h4 class='panel-title'><a id=\"toggley-thing"+team_number+"\" class='accordion-toggle' data-toggle='collapse' data-parent='#accordion' href='#my-collapse" + team_number + "'>"+ rank +". Team " + team_number +": " + name + "</a></h4></div><div id='my-collapse" + team_number + "'class='panel-collapse collapse'> <div class='panel-body table-body' style='overflow:auto;'><span><h1 class=\"team_num\">"+ team_number +"</h1><div class=\"graph_div\"><div id='graph"+ team_number+"' class='graph'></div></div></span><br><table class='table'><thead><tr id='erste" + team_number + "'><th>Team Number</th></tr></thead><tbody id='dritte" + team_number + "'><tr id='zweite"+ team_number +"0'><td>" + team_number+"</td></tr></tbody></table></div></div>" );
 	//	alert("#my-collapse"+team_number);
-	$("#toggley-thing"+team_number).click(function() {
+	$("#graph"+team_number).click(function() {
 		$.jqplot('graph'+team_number, [graph_array], { title: "Points Graph", axes: {xaxis: {min: 0}, yaxis: {min:0}}});
-    });
-    $.map(matches, function(match,match_number){
-
+	});
+	$.map(matches, function(match,match_number){
 		if(match_number == 0) {
 			//if i == 0 for if first
 			$.map(match, function(header,key) {
@@ -23,24 +33,23 @@ function new_team(team_number, matches, name, rank) {
 				$("#erste" + team_number ).append("<th " + ((key == "notes") ? "style=\"width: 250px;\"" : "") + ">" + key +"</th>");
 				$("#zweite" + team_number + match_number).append("<td>" + header + "</td>");
 				if (key == "teleop_shots") {
-				    graph_array[graph_count] = header;
-				    graph_count++;
+					graph_array[graph_count] = header;
+					graph_count++;
 				}
 			});
 		}
-
 		else {
 			$("#dritte" + team_number).append("<tr id='zweite"+ team_number + match_number + "'><td>" + team_number+ "</td></tr>");
 			$.map(match, function(header,key) {
 				$("#zweite" + team_number + match_number).append("<td>" + header + "</td>");
-			    if (key == "teleop_shots") {
-				    graph_array[graph_count] = header;
-				    graph_count++;
+				if (key == "teleop_shots") {
+					graph_array[graph_count] = header;
+					graph_count++;
 				}
 
 			});
 		}
-    });
+	});
 }
 
 
